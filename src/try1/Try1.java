bc27c9b0e34344acdf32e89460482d9ab7689a76
Baraa Arda <Baraa Arda@DESKTOP-1IIMENI>
2019-02-12 04:23:16 +0700

    first commit

---
+++ src/try1/Try1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package try1;
+
+/**
+ *
+ * @author Baraa Arda
+ */
+public class Try1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        int z;
+        z=x+y;
+        System.out.println("Hello");
+        System.out.println(z);
+        
+        // TODO code application logic here
+    }
+    
+}
