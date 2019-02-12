49015f4ad1e9424319ffe2368149b1bd2a2387a5
BaraaArda <47538186+BaraaArda@users.noreply.github.com>
2019-02-13 03:00:09 +0700

    addimg

---
+++ src/try1/Try1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package try1;
+
+/**
+ *
+ * @author Baraa Arda
+ */
+public class Try1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=6;
+        int y=11;
+        int z;
+        z=x+y;
+        System.out.println("Hello");
+        
+        
+        System.out.println(z);
+        
+        // TODO code application logic here
+    }
+    
+}
